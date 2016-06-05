c36286128d09ff42d95f608a1110b9c831b6c88e
vardhan <vardhan@vardhan-PC>
2016-06-06 00:40:12 +0530

    Initial Commit

---
+++ consulting-web/src/main/java/com/vardhan/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.vardhan.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author vardhan
+ *
+ */
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping("/index")
+	public ModelAndView home() {
+		logger.debug("home() is executed", "vardhan");
+
+		return new ModelAndView("index");
+	}
+	
+	@RequestMapping("/")
+	public ModelAndView index() {
+		logger.debug("index() is executed", "vardhan");
+
+		return new ModelAndView("index");
+	}
+}
---
+++ consulting-web/webapp/resources/js/style-switcher.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+	
+		// Color Changer
+		$("#preset1" ).click(function(){
+			$("#style-switch").attr("href", "resources/style/presets/preset1.css" );
+			return false;
+		});
+		$("#preset2" ).click(function(){
+			$("#style-switch").attr("href", "resources/style/presets/preset2.css" );
+			return false;
+		});
+		$("#preset3" ).click(function(){
+			$("#style-switch").attr("href", "resources/style/presets/preset3.css" );
+			return false;
+		});
+		$("#preset4" ).click(function(){
+			$("#style-switch").attr("href", "resources/style/presets/preset4.css" );
+			return false;
+		});
+		$("#preset5" ).click(function(){
+			$("#style-switch").attr("href", "resources/style/presets/preset5.css" );
+			return false;
+		});
+		$("#preset6" ).click(function(){
+			$("#style-switch").attr("href", "resources/style/presets/preset6.css" );
+			return false;
+		});
+
+});
