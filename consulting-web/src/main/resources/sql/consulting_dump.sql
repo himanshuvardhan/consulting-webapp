0c8f3e6dd1b31f44ff3ca0b20f310c13ecd2781d
vardhan <vardhan@vardhan-PC>
2016-12-11 21:41:44 +0530

    new table script

--- consulting-web/src/main/resources/sql/consulting_dump.sql
+++ consulting-web/src/main/resources/sql/consulting_dump.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.22, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.14, for Win64 (x86_64)
 --
 -- Host: localhost    Database: consulting
 -- ------------------------------------------------------
--- Server version	5.6.22-log
+-- Server version	5.7.14-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -37,7 +37,7 @@ CREATE TABLE `application_config` (
 
 LOCK TABLES `application_config` WRITE;
 /*!40000 ALTER TABLE `application_config` DISABLE KEYS */;
-INSERT INTO `application_config` VALUES (1,'application_style','preset4.css','UI');
+INSERT INTO `application_config` VALUES (1,'application_style','preset2.css','UI'),(2,'ENTERPRENEUR_SERVED','200','UI_METRIC'),(3,'PROFESSIONAL_NETWORK','77','UI_METRIC'),(4,'GEOGRAPHIES','7','UI_METRIC'),(5,'TEAM_STRENGTH','27','UI_METRIC');
 /*!40000 ALTER TABLE `application_config` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +94,7 @@ CREATE TABLE `companies` (
 
 LOCK TABLES `companies` WRITE;
 /*!40000 ALTER TABLE `companies` DISABLE KEYS */;
-INSERT INTO `companies` VALUES (1,'Private Limited',11999.00,NULL,NULL,NULL),(2,'Limited Liability Partnership',7999.00,NULL,NULL,NULL),(3,'One Person Company',10999.00,NULL,NULL,NULL),(4,'Public Limited Company',39999.00,NULL,NULL,NULL),(5,'Nidhi Limited Company',84999.00,NULL,NULL,NULL),(6,'Section 8 Company',25000.00,NULL,NULL,NULL);
+INSERT INTO `companies` VALUES (1,'Private Limited',12999.00,NULL,NULL,NULL),(2,'Limited Liability Partnership',9999.00,NULL,NULL,NULL),(3,'One Person Company',11999.00,NULL,NULL,NULL),(4,'Public Limited Company',42999.00,NULL,NULL,NULL),(5,'Nidhi Limited Company',74999.00,NULL,NULL,NULL),(6,'Section 8 Company',25000.00,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `companies` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -174,6 +174,7 @@ CREATE TABLE `income_tax_request` (
   `requestor_phone_number` varchar(30) DEFAULT NULL,
   `requestor_bank_name` varchar(45) DEFAULT NULL,
   `requestor_ifsc_code` varchar(45) DEFAULT NULL,
+  `user_type` varchar(45) DEFAULT NULL,
   `created_dt` datetime DEFAULT NULL,
   `updated_dt` datetime DEFAULT NULL,
   PRIMARY KEY (`income_tax_request_id`)
@@ -244,6 +245,90 @@ LOCK TABLES `loan_types` WRITE;
 INSERT INTO `loan_types` VALUES ('Home Loan','Home Loan',1),('Loan Against Property','Loan Against Property',2),('Balance Transfer & Top ups','Balance Transfer & Top ups',3),('Educational Loan','Educational Loan',4),('Term Loan Synidation','Term Loan Synidation',5);
 /*!40000 ALTER TABLE `loan_types` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `registration_service_request`
+--
+
+DROP TABLE IF EXISTS `registration_service_request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registration_service_request` (
+  `registration_service_request_id` int(11) NOT NULL AUTO_INCREMENT,
+  `requestor_full_name` varchar(50) DEFAULT NULL,
+  `requestor_phone_number` varchar(15) DEFAULT NULL,
+  `requestor_email_id` varchar(75) DEFAULT NULL,
+  `requestor_pan_number` varchar(20) DEFAULT NULL,
+  `created_dt` datetime DEFAULT NULL,
+  `updated_dt` datetime DEFAULT NULL,
+  `registration_service_type_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`registration_service_request_id`),
+  KEY `registration_service_fid_foreign_key_idx` (`registration_service_type_id`),
+  CONSTRAINT `registration_service_fid_foreign_key` FOREIGN KEY (`registration_service_type_id`) REFERENCES `registration_service_type` (`service_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registration_service_request`
+--
+
+LOCK TABLES `registration_service_request` WRITE;
+/*!40000 ALTER TABLE `registration_service_request` DISABLE KEYS */;
+/*!40000 ALTER TABLE `registration_service_request` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `registration_service_type`
+--
+
+DROP TABLE IF EXISTS `registration_service_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registration_service_type` (
+  `service_id` int(11) NOT NULL AUTO_INCREMENT,
+  `service_name` varchar(45) NOT NULL,
+  `service_type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`service_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registration_service_type`
+--
+
+LOCK TABLES `registration_service_type` WRITE;
+/*!40000 ALTER TABLE `registration_service_type` DISABLE KEYS */;
+INSERT INTO `registration_service_type` VALUES (1,'FSSAI Food License','license'),(2,'IEC Registration','registration'),(3,'Trade License','license'),(4,'ISO Registration','license'),(5,'Digital Signature (DSC)','license'),(6,'Employee State Insurance (ESI) Registration','license'),(7,'Employees Provident Fund','license'),(8,'Service Tax Registration','registration'),(9,'Sales Tax Registration','registration'),(10,'Professional Tax Registration','registration'),(11,'Excise Tax Registration','registration');
+/*!40000 ALTER TABLE `registration_service_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_query`
+--
+
+DROP TABLE IF EXISTS `user_query`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_query` (
+  `query_id` int(11) NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(55) DEFAULT NULL,
+  `subject` varchar(100) DEFAULT NULL,
+  `message` text,
+  `email_id` varchar(50) DEFAULT NULL,
+  `created_dt` datetime DEFAULT NULL,
+  `updated_dt` datetime DEFAULT NULL,
+  PRIMARY KEY (`query_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_query`
+--
+
+LOCK TABLES `user_query` WRITE;
+/*!40000 ALTER TABLE `user_query` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_query` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -254,4 +339,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-29 14:43:07
+-- Dump completed on 2016-12-11 21:36:16
